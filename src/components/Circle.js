e1701e9a84bd3ee1347bc59c6b50be9d8d65a082
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-10-21 21:51:11 +0530

    Initial commit

---
+++ src/components/Circle.js
@@ -0,0 +1,10 @@
+import Shape from "./Shape";
+
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea() {}
+}
+
+export default Circle;
---
+++ src/components/Shape.js
@@ -0,0 +1,10 @@
+export default class Shape {
+  color = null;
+
+  constructor(color) {
+    this.color = color;
+  }
+
+  drowShape = function () {};
+  calculateArea = function () {};
+}
